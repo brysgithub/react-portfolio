22ff62966328930ded5a2b3017b8cbd970f95e97
goblin.brn <bschenck0@gmail.com>
2022-05-26 19:15:45 -0700

    improved footer placement a little

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,11 +7,11 @@ function Footer() {
             <Row>
                 <Col>Github: <a href="https://github.com/brysgithub">brysgithub</a></Col>
                 <Col>LinkedIn: <a href="https://www.linkedin.com/in/bryan-schenck-4b56a1223/">Bryan Schenck</a></Col>
+                <Col>Call: (971) 867-4052 </Col>
                 <Col>Copyright &#169; 2022 Bryan Schenck </Col>
             </Row>
         </Container>
     )
-
 }
 
 export default Footer
